7693c54230bf24bce3b14af1ee0e2d802808ea83
Øivind Kolloen <okolloen@hig.no>
2014-02-24 15:18:44 +0100

    Selecting track and first visualization of that track

--- parsePSXFile.php
+++ parsePSXFile.php
@@ -1,4 +1,13 @@
 <?php
-foreach ($_FILES["files"] as $file) {
-	echo $file['tmp_name'];
+header ("Content-type: application/json");
+$t = file_get_contents($_FILES["files"]['tmp_name'][0]);
+$dom = simplexml_load_string ($t, 'SimpleXMLElement', LIBXML_NOCDATA);
+$data = array();
+foreach ($dom->trk as $track) {
+	$data[] = array("name" => (string)$track->name, "points"=>array());
+	foreach ($track->trkseg->trkpt as $point) {
+//		print_r($point);
+		$data[count($data)-1]["points"][] = array ("lat"=>(string)$point["lat"], "lon"=>(string)$point["lon"], "ele"=>(string)$point->ele, "time"=>(string)$point->time, "desc"=>(string)$point->desc);
+	}
 }
+echo json_encode ($data);
